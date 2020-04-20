417a0f8c3cb42b23519136150284cbfcbf2d2fb2
Brady Hammond <bradymh23@gmail.com>
2020-04-20 14:57:09 -0500

    First Commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+# ================================================== #
+#                       SETUP                        #
+# ================================================== #
+# Author: Brady Hammond                              #
+# Created: 04/20/2020                                #
+# Last Edited: N/A                                   #
+# ================================================== #
+#                      IMPORTS                       #
+# ================================================== #
+
+from setuptools import setup
+
+setup(
+    name='WikipediaGame',
+    version='0.0.1',
+    license='GPLv3',
+    description='Implementation of Wiki Game as outlined here: https://en.wikipedia.org/wiki/Wikipedia:Wiki_Game',
+    author='Brady Hammond',
+    author_email='bradymh23@gmail.com',
+    url='https://github.com/BradyHammond/wikipedia_game'
+)
